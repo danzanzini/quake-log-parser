81d9beaef4ce94c15c455155c99166357f8e8d9f
Daniel Zanzini <dpzanzini@gmail.com>
2024-04-30 11:21:48 -0300

    Get client_id from entry

--- lib/entry.rb
+++ lib/entry.rb
@@ -1,11 +1,20 @@
 # frozen_string_literal: true
 class Entry
-  attr_accessor :type
+  # TODO: Use types to define cases at main.rb
+  TYPES = [:client_begin, :client_connect, :client_userinfo_changed, :kill, :init_game]
+
+  attr_reader :type, :client_id
+
   def initialize(log_line)
     @log_line = log_line
+    @log_array = log_line.split
   end
 
   def type
-    @type ||= @log_line.split[1].gsub(':', '').underscore.to_sym
+    @type ||= @log_array[1].gsub(':', '').underscore.to_sym
+  end
+
+  def client_id
+    @client_id ||= [:client_begin, :client_connect, :client_userinfo_changed].include?(type) ? @log_array[2].to_i : nil
   end
 end
--- test/lib/entry_test.rb
+++ test/lib/entry_test.rb
@@ -26,4 +26,16 @@ class EntryTest < Minitest::Test
     entry = Entry.new('20:37 InitGame: \sv_floodProtect\1\sv_maxPing\0\sv_minPing\0\sv_maxRate\10000\sv_minRate\0\sv_hostname\Code Miner Server\g_gametype\0\sv_privateClients\2\sv_maxclients\16\sv_allowDownload\0\bot_minplayers\0\dmflags\0\fraglimit\20\timelimit\15\g_maxGameClients\0\capturelimit\8\version\ioq3 1.36 linux-x86_64 Apr 12 2009\protocol\68\mapname\q3dm17\gamename\baseq3\g_needpass\0')
     assert entry.type == :init_game
   end
+
+  # TODO: Improve client_id tests to ensure them for all types
+  def test_it_returns_the_client_id_when_appropriate
+    entry = Entry.new('21:15 ClientUserinfoChanged: 2 n\Isgalamido\t\0\model\uriel/zael\hmodel\uriel/zael\g_redteam\\g_blueteam\\c1\5\c2\5\hc\100\w\0\l\0\tt\0\tl\0')
+    assert entry.client_id == 2
+  end
+
+  def test_it_returns_nil_for_client_id_when_appropriate
+    entry = Entry.new('21:07 Kill: 1022 2 22: <world> killed Isgalamido by MOD_TRIGGER_HURT')
+    assert entry.client_id == nil
+  end
+
 end
