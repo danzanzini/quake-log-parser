21e9c5122eca15562841bf0082f3d811ff3e8c02
Daniel Zanzini <dpzanzini@gmail.com>
2024-04-30 11:52:10 -0300

    Adds connect player method to Game

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,18 @@
 class Game
-  attr_accessor :shutdown
+  attr_reader :shutdown, :players
   def initialize
     @shutdown = false
+    @players = []
+  end
+
+  def connect_client(client_id)
+    @players.append(Player.new(client_id)) if @players.none? { |p| p.id == client_id }
+  end
+
+  class Player
+    attr_reader :id
+    def initialize(id)
+      @id = id
+    end
   end
 end
--- test/lib/game_test.rb
+++ test/lib/game_test.rb
@@ -4,7 +4,20 @@ require_relative '../../lib/game'
 
 class GameTest < Minitest::Test
   def test_it_initializes_game_with_shutdown_false
-    @game = Game.new
-    assert !@game.shutdown
+    game = Game.new
+    assert !game.shutdown
+  end
+
+  def test_it_appends_player_when_it_does_not_exist
+    game = Game.new
+    game.connect_client(1)
+    assert game.players.count == 1
+  end
+
+  def test_it_does_not_append_player_when_it_exists
+    game = Game.new
+    game.connect_client(1)
+    game.connect_client(1)
+    assert game.players.count == 1
   end
 end
